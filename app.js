7d84615dda9358f13ddf2b54b685340021909203
Yuanchen Lu <ylu36@ncsu.edu>
2018-01-20 22:52:35 -0500

    option to display various days

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.config(function($routeProvider){
         templateUrl: 'pages/forecast.html',
         controller: 'forecastController'
     })
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
 });
 
 //services
@@ -28,12 +32,13 @@ app.controller('homeController', ['$scope', '$resource', 'cityService', function
   });
 }]);
 
-app.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
+app.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
   $scope.city = cityService.city;
+  $scope.days = $routeParams.days || 3;
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?APPID=4b986559c555b001f506ff8198500618", {
     callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
 
-  $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 3});
+  $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days});
   $scope.convertToF = function(degK) {
     return Math.round((1.8*(degK-273))+32);
   }
