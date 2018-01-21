9e868c20b7776cb46d99a63db266399e0d9eef72
Yuanchen Lu <ylu36@ncsu.edu>
2018-01-20 22:42:08 -0500

    display readable temperature

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 //module
 var app = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
@@ -21,13 +21,27 @@ app.service('cityService', function(){
 });
 
 //controllers
-app.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+app.controller('homeController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
   $scope.city = cityService.city;
   $scope.$watch('city', function(){
     cityService.city = $scope.city;
   });
 }]);
 
-app.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+app.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
   $scope.city = cityService.city;
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?APPID=4b986559c555b001f506ff8198500618", {
+    callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+
+  $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 3});
+  $scope.convertToF = function(degK) {
+    return Math.round((1.8*(degK-273))+32);
+  }
+  $scope.convertToC = function(degK) {
+    return Math.round(degK-273.15);
+  }
+  $scope.convertToDate = function(dt) {
+    return new Date(dt*1000);
+  }
+  console.log($scope.weatherResult);
 }]);
